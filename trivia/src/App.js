053c5051757d89cccb9001c9f03b634c031a3a55
Daniel <danielvillery@gmail.com>
2023-01-24 19:49:20 -0500

    category question is kind of working, but needs to be simplified

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 
+
+
 function App() {
+
+
  
   return (
     <div className="App">
@@ -10,6 +14,8 @@ function App() {
       </div>
   );
 }
+
+
 
 function Category() {
   const [categories, setCategories] = useState([])
@@ -21,11 +27,20 @@ function Category() {
       })
   },[])
 
+  const [selectedCat, setSelectedCat] = useState("")
+  
+  if (selectedCat){
+
+    console.log(selectedCat)
+  
+    return <CatQuestion />
+  }
+
   return (
     <div className='category-list'>
       <ul className='wrapper'>
        
-        {categories.map((cat) => (<li className='category' key={cat.id}>{cat.name}</li>
+        {categories.map((cat) => (<li onClick ={() =>setSelectedCat(cat.id)}className='category' key={cat.id}>{cat.name}</li>
         ))}
          
       </ul>
@@ -34,5 +49,39 @@ function Category() {
     </div>
   )
 }
+
+
+function CatQuestion(selectedCat) {
+  const [que, setQuestion] = useState([])
+  console.log(`cat id is ${selectedCat}`)
+  useEffect(() => {
+    axios
+    .get(`https://opentdb.com/api.php?amount=1&category=9`)
+    .then((response) =>{setQuestion(response.data.results.map(obj => [obj.question, obj.incorrect_answers, obj.correct_answer]))
+    })
+    
+  },[selectedCat])
+
+  
+
+  return (
+    <div className='question'>
+      {que.map((q) =>
+      (
+      <div>
+        <h1 key={selectedCat}>{q[0]}</h1>
+      <ul>
+      <li>{q[1][0]}</li>
+      <li>{q[1][1]}</li>
+      <li>{q[1][2]}</li>
+      <li>{q[2]}</li>
+      </ul>
+      </div>)
+   
+  )}
+  </div>
+  )
+
+      }
 
 export default App;
